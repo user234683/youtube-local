0dc5f411053bf75c5c895ead9a2a01b79dc3c898
James Taylor <user234683@users.noreply.github.com>
2018-07-27 18:55:35 -0700

    .

--- youtube/search.py
+++ youtube/search.py
@@ -58,7 +58,6 @@ def get_search_json(query, page, autocorrect, sort):
         'X-YouTube-Client-Version': '2.20180418',
     }
     url += "&pbj=1&sp=" + page_number_to_sp_parameter(page, autocorrect, sort).replace("=", "%3D")
-    print(url)
     content = common.fetch_url(url, headers=headers)
     info = json.loads(content)
     return info
