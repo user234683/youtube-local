80b325a22561758c4ef93e8d8a0c29db9de703bd
James Taylor <user234683@users.noreply.github.com>
2019-07-22 00:53:22 -0700

    Add homepage route

--- youtube/__init__.py
+++ youtube/__init__.py
@@ -1,3 +1,7 @@
 import flask
 yt_app = flask.Flask(__name__)
 yt_app.url_map.strict_slashes = False
+
+@yt_app.route('/')
+def homepage():
+    return flask.render_template('base.html', title="Youtube local")
