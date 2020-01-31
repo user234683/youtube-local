cd4a2fb0ebb63600d9e66fa695c6517a968a78f8
James Taylor <user234683@users.noreply.github.com>
2020-01-30 18:24:23 -0800

    run.bat: Allow command line usage from any directory Set the youtube-local directory as the working directory, and use setlocal so it doesn't affect the shell the command is being run from.

--- run.bat
+++ run.bat
@@ -1,5 +1,14 @@
 @echo off
 
+REM  https://stackoverflow.com/a/25719250
+REM  setlocal makes sure changing directory only applies inside this bat file,
+REM  and not in the command shell.
+setlocal
+
+REM  So this bat file can be called from a different working directory.
+REM  %~dp0 is the directory with this bat file.
+cd /d "%~dp0"
+
 REM  This is so brotli and gevent search in the python directory for the
 REM  visual studio c++ runtime dlls
 set PATH=.\python;%PATH%
