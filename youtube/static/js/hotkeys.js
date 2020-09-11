94ece08a1e04d9ba1f62000b04c7b7ca707c6ab6
zrose584 <57181548+zrose584@users.noreply.github.com>
2020-09-11 21:21:56 +0200

    hotkeys.js: add 'c' for transcript

--- youtube/static/js/hotkeys.js
+++ youtube/static/js/hotkeys.js
@@ -1,9 +1,7 @@
-Q = document.querySelector.bind(document);
-
 function onKeyDown(e) {
     if (['INPUT', 'TEXTAREA'].includes(document.activeElement.tagName)) return false;
 
-    console.log(e);
+    // console.log(e);
     let v = Q("video");
     let c = e.key.toLowerCase();
     if (c == "k") {
@@ -24,6 +22,18 @@ function onKeyDown(e) {
     else if (c == "l") {
         e.preventDefault();
         v.currentTime = v.currentTime + 10;
+    }
+    else if (c == "f") {
+        e.preventDefault();
+        if (document.fullscreen) document.exitFullscreen();
+        else v.requestFullscreen();
+    }
+    else if (c == "c") {
+        e.preventDefault();
+        let tt = getActiveTranscriptTrack();
+        if (tt == null) return;
+        if (tt.mode == "showing") tt.mode = "disabled";
+        else tt.mode = "showing";
     }
 }
 
