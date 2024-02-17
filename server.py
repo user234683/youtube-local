afbceec6730903e9eabf1a759a71bdfe290343e7
James Taylor <user234683@users.noreply.github.com>
2024-02-17 09:59:29 -0800

    server.py: Return headers as a list to start_response
    uwsgi doesn't like headers as dictionaries and wants them as python
    lists
    
    Fixes #198

--- server.py
+++ server.py
@@ -85,8 +85,12 @@ def proxy_site(env, start_response, video=False):
             response, cleanup_func = util.fetch_url_response(url, send_headers)
 
         response_headers = response.getheaders()
-        if isinstance(response_headers, urllib3._collections.HTTPHeaderDict):
-            response_headers = response_headers.items()
+        #if isinstance(response_headers, urllib3._collections.HTTPHeaderDict):
+        #   response_headers = response_headers.items()
+        try:
+            response_headers = list(response_headers.items())
+        except AttributeError:
+            pass
         if video:
             response_headers = (list(response_headers)
                                 +[('Access-Control-Allow-Origin', '*')])
