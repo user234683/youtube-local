49becf98aab6276efc5d735639ca4ef73f91ed6a
alive4ever <alive4ever@users.noreply.github.com>
2025-01-23 04:01:49 +0000

    yt_data_extract: import append_po_token
    Import the append_po_token function so the function is accessible from
    watch page.

--- youtube/yt_data_extract/__init__.py
+++ youtube/yt_data_extract/__init__.py
@@ -9,5 +9,5 @@ from .everything_else import (extract_channel_info, extract_search_info,
 from .watch_extraction import (extract_watch_info, get_caption_url,
     update_with_new_urls, requires_decryption,
     extract_decryption_function, decrypt_signatures, _formats, replace_n_signatures,
-    update_format_with_type_info, extract_hls_formats,
+    update_format_with_type_info, extract_hls_formats, append_po_token,
     extract_watch_info_from_html, captions_available)
